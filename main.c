3e9b683cdd997659f49dff5f1a831a8cb9fbd4d2
Mehdi <jafarizadeh89@gmail.com>
2023-08-19 17:00:57 +0200

    movePlayer

--- main.c
+++ main.c
@@ -10,138 +10,123 @@ typedef struct Player
 
 int screenSetUp();
 int mapSetUp();
-int handleInput (int input, Player * user);
-int playerMove(int y, int x, Player * user);
-Player * playerSetUp();
+void handleInput(int input, Player *user);
+void playerMove(int y, int x, Player *user);
+Player *playerSetUp();
 
-int main () 
+int main()
 {
-	Player * user;
+	Player *user;
 	int ch;
 
 	screenSetUp();
-	
+
 	mapSetUp();
-	
+
 	user = playerSetUp();
-	
+
 	// main game loop
 	while ((ch = getch()) != 'q')
 	{
 		handleInput(ch, user);
 	}
-	
+
 	endwin();
-	
+
 	return 0;
 }
 
-
-int screenSetUp() 
+int screenSetUp()
 {
 	initscr();
 	printw("Mehdi");
 	noecho();
 	refresh();
-	
+
 	return 1;
-	
 }
 
 int mapSetUp()
-{	
+{
 	mvprintw(13, 13, ",------,");
 	mvprintw(14, 13, "|......|");
 	mvprintw(15, 13, "|......|");
 	mvprintw(16, 13, "|......|");
 	mvprintw(17, 13, "|......|");
 	mvprintw(18, 13, "'------'");
-	
+
 	mvprintw(2, 40, ",------,");
 	mvprintw(3, 40, "|......|");
 	mvprintw(4, 40, "|......|");
 	mvprintw(5, 40, "|......|");
 	mvprintw(6, 40, "|......|");
 	mvprintw(7, 40, "'------'");
-	
+
 	mvprintw(10, 40, ",----------,");
 	mvprintw(11, 40, "|..........|");
 	mvprintw(12, 40, "|..........|");
 	mvprintw(13, 40, "|..........|");
 	mvprintw(14, 40, "|..........|");
 	mvprintw(15, 40, "'----------'");
-	
-	return 0;
 
+	return 0;
 }
 
-Player * playerSetUp()
+Player *playerSetUp()
 {
-	Player * newPlayer;
+	Player *newPlayer;
 	newPlayer = malloc(sizeof(Player));
-	
-	newPlayer -> xPosition = 14;
-	newPlayer -> yPosition = 14;
-	newPlayer -> health = 20;
-	
+
+	newPlayer->xPosition = 14;
+	newPlayer->yPosition = 14;
+	newPlayer->health = 20;
+
+	mvprintw(newPlayer-> yPosition, newPlayer-> xPosition, "@");
+	move(newPlayer-> yPosition, newPlayer -> xPosition);
+
 	return newPlayer;
 }
 
-int handleInput (int input, Player * user)
+void handleInput(int input, Player *user)
 {
 	switch (input)
 	{
-		// move up
-		case 'w' : 
-		case 'W' :
-			playerMove(user -> yPosition - 1, user -> xPosition, user);
-			break;
-			
-		// move down
-		case 's' : 
-		case 'S' :
-			playerMove(user -> yPosition + 1, user -> xPosition, user);
-			break;
-			
-		// move left
-		case 'a' : 
-		case 'A' :
-			playerMove(user -> yPosition, user -> xPosition, user - 1);
-			break;
-		
-		// move right	
-		case 'd' : 
-		case 'D' :
-			playerMove(user -> yPosition, user -> xPosition, user + 1);
-			break;
-			
-		default:
-			break;
+	// move up
+	case 'w':
+	case 'W':
+		playerMove(user->yPosition - 1, user->xPosition, user);
+		break;
+
+	// move down
+	case 's':
+	case 'S':
+		playerMove(user->yPosition + 1, user->xPosition, user);
+		break;
+
+	// move left
+	case 'a':
+	case 'A':
+		playerMove(user->yPosition, user->xPosition - 1, user);
+		break;
+
+	// move right
+	case 'd':
+	case 'D':
+		playerMove(user->yPosition, user->xPosition + 1, user);
+		break;
+
+	default:
+		break;
 	}
 }
 
-int playerMove (int y, int x, Player * user)
+void playerMove(int y, int x, Player *user)
 {
-	mvprintw(user -> yPosition, user -> xPosition, ".");
-	
-	user -> yPosition = y;
-	user -> xPosition = x;
-	
-	mvprintw(user -> yPosition, user -> xPosition, "@");
-	move(user -> yPosition, user -> xPosition);
+	mvprintw(user->yPosition, user->xPosition, ".");
+
+	user->yPosition = y;
+	user->xPosition = x;
+
+	mvprintw(user->yPosition, user->xPosition, "@");
+	move(user->yPosition, user->xPosition);
 }
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
